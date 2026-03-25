77cbb6a45f417666d9cfd96c60a929b70beda51b
Anand kumar padakala <anandkumarpadakala786@gmail.com>
2026-03-25 10:55:26 +0530

    Version 1 - Backend core completed with README

---
+++ Backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+const app = express();
+
+// connect DB
+connectDB();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+module.exports = app;
